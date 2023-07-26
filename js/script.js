5f3080f06ecfc0082921e9b97976ac821b387e7c
JuanToscanini <juanmtosca@gmail.com>
2023-07-26 19:32:42 -0300

    TP Final Javascript Toscanini

--- js/script.js
+++ js/script.js
@@ -1,35 +1,4 @@
-// getProduct()
-// class Perfume {
-//     constructor(nombre, marca, tamanio, genero, tipo, precio, id, stock, rutaImagen) {
-//         this.nombre = nombre;
-//         this.marca = marca;
-//         this.tamanio = tamanio;
-//         this.genero = genero;
-//         this.tipo = tipo;
-//         this.precio = precio;
-//         this.id = id;
-//         this.stock = stock
-//         this.rutaImagen = rutaImagen;
-//     }
 
-//     agregarItem() {
-//         carrito.agregarPerfume(this);
-//     }
-// }
-
-// const perfumes = [
-//     new Perfume('Boss Bottled', 'HUGO BOSS', '50', 'masculino', 'Parfum', '43300', '1', '18', "bossbottled.jpg"),
-//     new Perfume('Boss Alive', 'HUGO BOSS', '50', 'femenino', 'Parfum', '44000', '2', '25', "bossalive1.jpg"),
-//     new Perfume('Boss Alive', 'HUGO BOSS', '80', 'femenino', 'Parfum', '57000', '3', '11', "bossalive2.jpg"),
-//     new Perfume('ACQUA DI GIO HOMME ', 'ARMANI', '100', 'masculino', 'Toilette', '48900', '4', '21', "acqua1.jpeg"),
-//     new Perfume('ACQUA DI GIO HOMME ABSOLU', 'ARMANI', '200', 'masculino', 'Parfum', '64000', '5', '13', "acquaabsolu.jpg"),
-//     new Perfume('One Million', 'Paco Rabanne', '100', 'masculino', 'Toilette', '47700', '6', '11', "onemillion1.jpg"),
-//     new Perfume('One Million', 'Paco Rabanne', '200', 'masculino', 'Toilette', '69200', '7', '8', "onemillion2.jpg"),
-//     new Perfume('Invictus', 'Paco Rabanne', '50', 'masculino', 'Toilette', '40500', '8', '16', "invictus.jpg"),
-//     new Perfume('Invictus', 'Paco Rabanne', '100', 'masculino', 'Toilette', '47500', '9', '10', "invictus1.jpg"),
-//     new Perfume('Bad Boy', 'Carolina Herrera', '50', 'masculino', 'Parfum', '44900', '10', '18', "badboy.jpg"),
-//     new Perfume('Eros', 'Versace', '200', 'masculino', 'Toilette', '68400', '11', '12', "eros.jpg")
-// ];
 getProduct()
 async function getProduct() {
     const productosjson = await fetch("./js/productos.JSON")
@@ -40,12 +9,12 @@ async function getProduct() {
 
 
 // funciones para decorar 
-// const bienvenida = document.getElementById('bienvenidaId');
-// function ocultarElemento() {
-//     bienvenida.style.display = 'none'
-// }
-// setTimeout(ocultarElemento, 20000);
-// contenido de DOM
+const bienvenida = document.getElementById('bienvenidaId');
+function ocultarElemento() {
+    bienvenida.style.display = 'none'
+}
+setTimeout(ocultarElemento, 7000);
+
 
 let carritoA = []
 let contenedorPerfumes = document.getElementById("contenedor");
@@ -140,12 +109,13 @@ async function reiniciarStock() {
 function guardarCarritoEnLocalStorage() {
     localStorage.setItem("carritoA", JSON.stringify(carritoA));
 }
+
 function renderizarCarrito() {
-  let carritoFisico = document.getElementById("carrito");
-  let i = 0
-  carritoFisico.innerHTML = "";
-  carritoA.forEach(perfume => {
-    carritoFisico.innerHTML += `
+    let carritoFisico = document.getElementById("carrito");
+    let i = 0
+    carritoFisico.innerHTML = "";
+    carritoA.forEach(perfume => {
+        carritoFisico.innerHTML += `
       <div class="col">
         <div class="card">
           <div class="row">
@@ -156,11 +126,11 @@ function renderizarCarrito() {
         </div>
       </div>
     `;
-    i++
-  });
+        i++
+    });
 
-  quitarFuncion();
-  actualizarCarritoEnLocalStorage();
+    quitarFuncion();
+    actualizarCarritoEnLocalStorage();
 }
 
 function limpiarLocalStorage() {
@@ -194,13 +164,15 @@ let boton = document.getElementById("miBoton");
 
 input.addEventListener("input", () => filtrarYRenderizar(input.value));
 
+// filtrar y renderizar 
+
 async function filtrarYRenderizar(valorFiltro) {
     const productosjson = await fetch("./js/productos.JSON")
     const perfumes = await productosjson.json()
     let perfumesFiltrados = perfumes.filter(perfume => perfume.nombre.toLowerCase().includes(valorFiltro))
     renderizarConFiltro(perfumesFiltrados)
 }
-//
+// filtrar y renderizar 
 async function filtrarYRenderizarPorCategoriaGenero(e) {
     const productosjson = await fetch("./js/productos.JSON")
     const perfumes = await productosjson.json()
@@ -223,10 +195,11 @@ for (const botonFiltro of botonesFiltros) {
 for (const botonFiltro of botonesFiltroTipo) {
     botonFiltro.addEventListener("click", filtrarYRenderizarPorCategoriaTipo)
 }
-
+// funcion actualizar
 function actualizarCarritoEnLocalStorage() {
     localStorage.setItem('carritoA', JSON.stringify(carritoA));
-  }
+}
+
 
 function quitarDelCarrito(id) {
     const index = carritoA.findIndex((item) => item.id === id);
@@ -236,44 +209,44 @@ function quitarDelCarrito(id) {
         renderizarCarrito();
     }
 }
-//Funcion para quitar un producto del carrito//
+// funcion quitar un producto del carrito//
 function quitarFuncion() {
     let i = 0;
     carritoA.forEach((element) => {
-      let idBtnQuitar = 'quitar' + i;
-      const btnQuitar = document.getElementById(idBtnQuitar);
-      btnQuitar.addEventListener('click', (e) => {
-        e.preventDefault();
-        Swal.fire({
-          title: 'Eliminar producto',
-          text: '¿Esta seguro de elimiar el producto?',
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Si',
-          cancelButtonText: 'no'
-        })
-          .then((result) => {
-            if (result.isConfirmed) {
-              quitarDelCarrito(element.id);
-              Swal.fire('Borrado', 'El producto ha sido eliminado', 'success')
-            }
-          })
-      });
-      i++;
+        let idBtnQuitar = 'quitar' + i;
+        const btnQuitar = document.getElementById(idBtnQuitar);
+        btnQuitar.addEventListener('click', (e) => {
+            e.preventDefault();
+            Swal.fire({
+                title: 'Eliminar producto',
+                text: '¿Esta seguro de eliminar el producto?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Si',
+                cancelButtonText: 'no'
+            })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        quitarDelCarrito(element.id);
+                        Swal.fire('Borrado', 'El producto ha sido eliminado', 'success')
+                    }
+                })
+        });
+        i++;
     });
-  }
+}
 
 
-  let botonCarrito = document.getElementById("botonCarrito")
-  botonCarrito.addEventListener("click", mostrarOcultar)
-  let botonVolverAProductos = document.getElementById("volverAProductos")
-  botonVolverAProductos.addEventListener("click", mostrarOcultar)
-  let botonFiltroGenero1 = document.getElementById("filtroGenero1") 
-  let botonFiltroGenero2 = document.getElementById("filtroGenero2")
-  let botonFiltroTipo1 = document.getElementById("filtroTipo1")
-  let botonFiltroTipo2 = document.getElementById("filtroTipo2")
-  let fraganciasP = document.getElementById("fraganciasP")
-  let Capa_1 = document.getElementById("Capa_1")
+let botonCarrito = document.getElementById("botonCarrito")
+botonCarrito.addEventListener("click", mostrarOcultar)
+let botonVolverAProductos = document.getElementById("volverAProductos")
+botonVolverAProductos.addEventListener("click", mostrarOcultar)
+let botonFiltroGenero1 = document.getElementById("filtroGenero1")
+let botonFiltroGenero2 = document.getElementById("filtroGenero2")
+let botonFiltroTipo1 = document.getElementById("filtroTipo1")
+let botonFiltroTipo2 = document.getElementById("filtroTipo2")
+let fraganciasP = document.getElementById("fraganciasP")
+let Capa_1 = document.getElementById("Capa_1")
 
 function mostrarOcultar() {
     let padreContenedor = document.getElementById("productos")
@@ -289,7 +262,40 @@ function mostrarOcultar() {
     botonFiltroTipo2.classList.toggle("oculto")
     fraganciasP.classList.toggle("oculto")
     Capa_1.classList.toggle("oculto")
-    
+
 }
 
 renderizar()
+
+// getProduct()
+// class Perfume {
+//     constructor(nombre, marca, tamanio, genero, tipo, precio, id, stock, rutaImagen) {
+//         this.nombre = nombre;
+//         this.marca = marca;
+//         this.tamanio = tamanio;
+//         this.genero = genero;
+//         this.tipo = tipo;
+//         this.precio = precio;
+//         this.id = id;
+//         this.stock = stock
+//         this.rutaImagen = rutaImagen;
+//     }
+
+//     agregarItem() {
+//         carrito.agregarPerfume(this);
+//     }
+// }
+
+// const perfumes = [
+//     new Perfume('Boss Bottled', 'HUGO BOSS', '50', 'masculino', 'Parfum', '43300', '1', '18', "bossbottled.jpg"),
+//     new Perfume('Boss Alive', 'HUGO BOSS', '50', 'femenino', 'Parfum', '44000', '2', '25', "bossalive1.jpg"),
+//     new Perfume('Boss Alive', 'HUGO BOSS', '80', 'femenino', 'Parfum', '57000', '3', '11', "bossalive2.jpg"),
+//     new Perfume('ACQUA DI GIO HOMME ', 'ARMANI', '100', 'masculino', 'Toilette', '48900', '4', '21', "acqua1.jpeg"),
+//     new Perfume('ACQUA DI GIO HOMME ABSOLU', 'ARMANI', '200', 'masculino', 'Parfum', '64000', '5', '13', "acquaabsolu.jpg"),
+//     new Perfume('One Million', 'Paco Rabanne', '100', 'masculino', 'Toilette', '47700', '6', '11', "onemillion1.jpg"),
+//     new Perfume('One Million', 'Paco Rabanne', '200', 'masculino', 'Toilette', '69200', '7', '8', "onemillion2.jpg"),
+//     new Perfume('Invictus', 'Paco Rabanne', '50', 'masculino', 'Toilette', '40500', '8', '16', "invictus.jpg"),
+//     new Perfume('Invictus', 'Paco Rabanne', '100', 'masculino', 'Toilette', '47500', '9', '10', "invictus1.jpg"),
+//     new Perfume('Bad Boy', 'Carolina Herrera', '50', 'masculino', 'Parfum', '44900', '10', '18', "badboy.jpg"),
+//     new Perfume('Eros', 'Versace', '200', 'masculino', 'Toilette', '68400', '11', '12', "eros.jpg")
+// ];
